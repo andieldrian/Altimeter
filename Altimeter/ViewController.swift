38be88038990880c766164f6eb4708e3ffd64a1d
Eldrian <andieldrian@gmail.com>
2019-07-10 14:42:31 +0700

    Testing

--- Altimeter/ViewController.swift
+++ Altimeter/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var relativeAltitude: UILabel!
+    
+    
+    let altimeter = CMAltimeter()
+    
+    @IBAction func startButton(_ sender: Any) {
+        startAltimeter()
+    }
+    @IBAction func stopButton(_ sender: Any) {
+        altimeter.stopRelativeAltitudeUpdates()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func startAltimeter() {
+        if CMAltimeter.isRelativeAltitudeAvailable() {
+            altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main) { (data, error) in
+                self.relativeAltitude.text = data?.relativeAltitude.stringValue
+//                self.pressure.text = data?.pressure.stringValue
+            }
+        }
     }
 
 
